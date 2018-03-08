a9a5e3adfcefb02a29329af7d7f4761d553bdd89
raghavendra188 <raghavendra.rao@novopay.in>
2018-03-08 20:48:03 +0530

    first commit
    first commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,7 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
+	  System.out.println("I am Raghavendra and I have started Jenkins.");
     return String.format("Hello, %s!", someone);
   }
 }
